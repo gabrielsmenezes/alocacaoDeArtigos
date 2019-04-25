0f80f7119e97bf0c5022e5e3b24cc1b482d202f6
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-04-25 10:00:39 -0400

    Teste para criacao dos Revisores e para validacao de todos artigos com revisor

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import Revisor
 def lerArquivoDeEntrada(nomeDoArquivo):
     arquivo = open(nomeDoArquivo)
     matriz = []
@@ -8,3 +9,19 @@ def lerArquivoDeEntrada(nomeDoArquivo):
             vetor.append(int(casa))
         matriz.append(vetor)
     return matriz
+
+
+def criarRevisores(matriz):
+    revisores = []
+    for vetor in matriz:
+        revisor = Revisor.Revisor(listaDeAfinidades=vetor[:len(vetor)-1], quantidadeMaximaDeArtigos=vetor[len(vetor)-1])
+        revisores.append(revisor)
+    return revisores
+
+def todosArtigosComRevisor(artigos):
+    return not -1 in artigos
+
+# def validarEstado();
+#     #nenhum artigo sem revisor
+#     todosArtigosComRevisor(artigo)
+#     #nenhum revisor com mais artigos que o maximo que ele pode
